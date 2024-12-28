6774b3956e46a6b66fe669468770bdd4e50df56b
Deyi Rao <deyi.rao@gmx.de>
2024-12-28 17:29:06 +0100

    Almost complete navbar, what is left to do is modify hover and background colors

--- development/js/script.js
+++ development/js/script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.querySelector('.nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+    const closeIcon = document.querySelector('.close-icon');
 
     hamburger.addEventListener('click', function() {
         navLinks.classList.toggle('active');
+        menuIcon.classList.toggle('hidden');
+        closeIcon.classList.toggle('hidden');
     });
 });
