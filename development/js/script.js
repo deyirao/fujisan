776502feb4b037df5fd2a4fa70eb37bca79de147
Deyi Rao <deyi.rao@gmx.de>
2024-12-28 20:40:21 +0100

    Complete navigation bar section

--- development/js/script.js
+++ development/js/script.js
@@ -3,10 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelector('.nav-links');
     const menuIcon = document.querySelector('.menu-icon');
     const closeIcon = document.querySelector('.close-icon');
+    const navItems = document.querySelectorAll(".nav-menu li");
 
     hamburger.addEventListener('click', function() {
         navLinks.classList.toggle('active');
+
         menuIcon.classList.toggle('hidden');
         closeIcon.classList.toggle('hidden');
+
+        navItems.forEach((item, index) => {
+            item.style.setProperty('--i', index);
+        });
     });
 });
