b7b6984d4f3b3e4f5319c6f185bb5d399d8f4d4f
Deyi Rao <deyi.rao@gmx.de>
2025-01-05 15:46:17 +0100

    Finish swiper in gallery section

--- development/js/swiper-config.js
+++ development/js/swiper-config.js
@@ -7,10 +7,6 @@ const swiper = new Swiper('.swiper-container', {
         delay: 3000,
         disableOnInteraction: false,
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
